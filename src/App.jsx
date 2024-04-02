27c8bbe38d9c0c85d110dd7eb30b82247846a193
arfanur04 <arfanur04@gmail.com>
2024-04-02 15:51:10 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import "./App.css";
+import GoogleMapReact from "google-map-react";
+import { googleApiKey } from "./googleAPIKey";
+import { IoLocationSharp } from "react-icons/io5";
+
+const AnyReactComponent = (/* { text } */) => (
+	<div style={{ color: "red", fontSize: "40px" }}>
+		<IoLocationSharp />
+	</div>
+);
+
+function App() {
+	const defaultProps = {
+		center: {
+			lat: 25.716949,
+			lng: 89.26395,
+		},
+		zoom: 15,
+	};
+	return (
+		<>
+			<div style={{ height: "500px", width: "500px" }}>
+				<GoogleMapReact
+					bootstrapURLKeys={{ key: googleApiKey }}
+					defaultCenter={defaultProps.center}
+					defaultZoom={defaultProps.zoom}
+				>
+					<AnyReactComponent
+						lat={defaultProps.center.lat}
+						lng={defaultProps.center.lng}
+						// text="My Marker"
+					/>
+				</GoogleMapReact>
+			</div>
+		</>
+	);
+}
+
+export default App;
